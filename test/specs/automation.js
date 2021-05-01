55dbbfa9c1efa6f560f39afcbc1196528cdc78c2
Melisa Bellico <melisabellico@gmail.com>
2021-05-01 20:10:26 -0300

    automation.js file updated

--- test/specs/automation.js
+++ test/specs/automation.js
@@ -7,17 +7,16 @@ describe('Google Page', () => {
     it('should search for the "Wikipedia" web page', () => {
         const searchInput = $('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input');
         browser.pause(2000);
-        const searchBtn = $('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.tfB0Bf > center > input.gNO89b');
-        searchInput.addValue('http://wikipedia.com');
-        searchBtn.click();
+        searchInput.addValue('wikipedia');
+        browser.keys('Enter');
     });
     it('should enter the "Wikipedia" site', () => {
-        const elem = $('#rso > div:nth-child(1) > div:nth-child(2) > div > div > div.yuRUbf > a > h3');
+        const elem = $('#rso div:nth-child(1)  div:nth-child(1) div div.tF2Cxc div.yuRUbf a');
         browser.pause(2000);
         elem.click();
     })
     it('Should validate the opened site', () => {
         browser.url('https://es.wikipedia.org/wiki/Wikipedia:Portada');
-        browser.pause(2000);
+        browser.pause(1000);
         expect(browser).toHaveUrl('https://es.wikipedia.org/wiki/Wikipedia:Portada');
-    })
+    });
